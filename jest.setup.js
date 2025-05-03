92b8693befa728e1dff09d6eb3c9b706d376d181
Janan Al-Hassani <janan.alhassani@gmail.com>
2025-05-03 19:18:48 -0400

    Mock matchMedia

--- jest.setup.js
+++ jest.setup.js
@@ -5,3 +5,17 @@ import { TextEncoder, TextDecoder } from "util";
 
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
+
+Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation(query => ({
+        matches: false, // or true if you want to simulate a match
+        media: query,
+        onchange: null,
+        addListener: jest.fn(),    // deprecated
+        removeListener: jest.fn(), // deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+    })),
+});
